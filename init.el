568b1af3db39bcfbead29f737daca82224316833
Side <sidew@qq.com>
2021-04-15 22:24:59 +0800

    first day to use emacs

---
+++ init.el
@@ -0,0 +1,15 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(tool-bar-mode -1)
+
+(scroll-bar-mode -1)
+
+(global-linum-mode 1)
+
+(setq inhibit-splash-screen 1)
+
